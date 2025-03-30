80f55ed637b74a81c1be26b5e1f8fae64fe8c642
raghuram.d <raghuram.d@tavant.com>
2025-03-30 07:14:13 +0530

    test

--- scoreboard.js
+++ scoreboard.js
@@ -759,7 +759,6 @@ function goBackToSeesionList() {
 
 function sendText() {
 	window.open('https://wa.me/919247591036?text=hello 2');
-	window.open('https://wa.me/916546546465?text=hello 1');
 	window.open('https://wa.me/918897994040?text=hello');
 }
 
